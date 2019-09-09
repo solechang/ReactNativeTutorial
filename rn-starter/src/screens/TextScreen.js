3637442f749af160e95e6b4fb3cdbd15b853b2b2
Chang Choi <changwoo92@gmail.com>
2019-09-09 12:06:57 -0700

    TextScreen

---
+++ rn-starter/src/screens/TextScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native'
+
+const TextScreen = () => {
+    return <View>
+        <Text>Text Screen</Text>
+    </View>
+};
+const styles = StyleSheet.create({});
+export default TextScreen;
